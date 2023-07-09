b8eac43ccd865780aaba0b6fcbd36ee0272c8753
Awab Saif <132479530+AwabSaif@users.noreply.github.com>
2023-07-09 23:50:10 +0300

    Add files via upload

---
+++ translations.js
@@ -0,0 +1,70 @@
+
+const translations= {
+    en: {
+        buy:"Buy", 
+        resources:"Resources",
+        explore:"Explore",
+        title: "Insanely Expensive JPEGs",
+        moreTitle: "(Also known as NFTs)",
+        productTitle: "Meta-Pigeon Sneaker NFT $33,000",
+        productTitleD: "At $33k, this NFT sneaker is super good value!",
+        productD1: `If you've got lots of money and you're a bit daft, NFTs are a 
+				great way to pay for something that doesn't have any intrinsic 
+				value or even exist. Tech fashionistas are jumping on the NFT 
+				bandwagon  crafting an NFT ,and spending eye-watering sums of money on crypto images. 
+				Our advice: spend the weekend `  ,
+        productd2: `and by this time next week, you'll be a millionaire.`,
+        btnAnNft: "crafting an NFT",
+        btnBuy: "Buy NFTs",
+        btnMoerInfo: "More info",
+        sectiontwotitle :"For the true Crypto-connoisseur",
+        sectiontwodimg1 :`Is $33k for sneakers too basic? No problem! This CryptoPunk is a steal at $6.6 million,
+        making Bag #748 (right) a total bargain.`,
+        sectiontwodimg2 :`That's not a screenshot. Those eight lines of white text on a black background is the actual
+        NFT we're selling for $1.4 million.` ,
+        aboutus :"About Us",
+        contact :"Contact",
+        sectiontwodbtn:"Still not convinced? To be honest, nor are we. That's why we're selling them, not buying them.",
+        footer:"2023 Awab aif",
+
+         selectLanguage: "Select a language",
+        english: "English",
+        arabic: "Arabic", 
+
+
+
+
+    },
+    ar: {
+        buy:"اشتري", 
+        resources:"موارد",
+        explore:"استكشف",
+        title: "JPEGs باهظة الثمن ",
+        moreTitle: "(المعروف أيضًا باسم NFTs)",
+        productTitle: "حذاء ميتابيجون أن اف تي 33000 دولار",
+        productTitleD: "بسعر 33 ألف دولار ، يعتبر هذا أن اف تي الحذاء الرياضي ذو قيمة جيدة للغاية!",
+        productD1: `إذا كان لديك الكثير من المال وكنت غبيًا بعض الشيء ، تعد ان اف تي طريقة رائعة للدفع مقابل شيء ليس له أي قيمة جوهرية أو حتى موجود. يقفز عشاق الموضة في عالم التكنولوجيا على
+        عربة ان اف تي وإنفاق مبالغ طائلة من المال على الصور المشفرة.
+          نصيحتنا: قضاء عطلة نهاية الأسبوع `  ,
+        productd2: `وبحلول هذا الوقت من الأسبوع المقبل ، ستكون مليونيرا.`,
+        btnAnNft: "صنع ان اف تي",
+        btnBuy: "اشتري ان اف تي",
+        btnMoerInfo: "مزيد من المعلومات",
+        sectiontwotitle :"لمتذوق التشفير الحقيقي",
+        sectiontwodimg1 :`هل تعتبر تكلفة السنيكرز أساسية للغاية 33 ألف دولار؟لا توجد مشكلة!هذه العملة المشفرة هي سرقة بقيمة 6.6 مليون دولار أمريكي، صنع الحقيبة رقم 748 (يمين) صفقة كاملة.`,
+        sectiontwodimg2 :`هذه ليست لقطة شاشة. تلك الأسطر الثمانية من النص الأبيض على خلفية سوداء هو بالحقيقة ان اف تي نبيعه بمبلغ 1.4 مليون دولار.` ,
+        aboutus :"من نحن",
+        contact :"اتصل بنا",
+        sectiontwodbtn:"ما زلت غير مقتنع؟ أن نكون صادقين ، ولا نحن كذلك. لهذا السبب نبيعها ، لا نشتريها.",
+        footer:"2023 أواب سيف",
+
+        selectLanguage: "إختر لغة",
+        english: "الانجليزية",
+        arabic: "العربية",
+        
+
+    },
+}
+
+
+export default translations;
